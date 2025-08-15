0007a9e6445866ef55835d3b2f1dacfcf88cf311
Daniella Pleta <daniellatpleta@iskolarngbayan.pup.edu.ph>
2025-08-16 07:45:34 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/group-2--Shipora/', 
+  base: '/group-2--Shipora/', // 👈 this must match your repo name exactly
 })
+
