60966a68531722fee6b78659632b49226dca0269
Daniella Pleta <daniellatpleta@iskolarngbayan.pup.edu.ph>
2025-08-16 09:00:48 +0800

    update

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -61,7 +61,7 @@ export default function AboutSection() {
 
             {/* Main image */}
             <img
-              src={`${base}images/Barko.png`}
+              src={`${base}images/barko.png`}
               alt="Cargo ship"
               className="rounded-2xl shadow-lg w-full h-[480px] object-cover"
             />
